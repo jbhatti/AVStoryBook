b3ca536c09d612137b7732ea9fcf339d56afb057
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-22 15:32:29 -0400

    add StoryPartViewController class

---
+++ AVStoryBook/StoryPartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryPartViewController.h
+//  AVStoryBook
+//
+//  Created by Jaison Bhatti on 2017-09-22.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryPartViewController : UIViewController
+
+@end
